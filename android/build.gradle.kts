c34e9384fa1335273d8b1628670eed353f22807a
presentVersion <tanzilbasha69@gmail.com>
2026-07-30 23:03:10 +0530

    Set Java and Kotlin compile compatibility to Java 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,9 +23,14 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
             jvmTarget = "17"
         }
     }
 }
+
