b122b44c620fbc6a934cb5035a8b9a6bdf54a578
presentVersion <tanzilbasha69@gmail.com>
2026-07-31 06:21:37 +0530

    Update compileSdk version to 37

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.habit_tracker"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
